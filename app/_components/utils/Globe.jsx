f3d63a9f23ae7097fbdac345eb17691efaea9231
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-11-07 00:59:11 +0200

    feat(style): :sparkles: Update SpinningGlobe component with dynamic width and height

--- app/_components/utils/Globe.jsx
+++ app/_components/utils/Globe.jsx
@@ -1,14 +1,20 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Globe from 'react-globe.gl';
 
 const SpinningGlobe = () => {
   const globeEl = useRef();
+  const [globeWidth, setGlobeWidth] = useState(850);
+  const [globeHeight, setGlobeHeight] = useState(700);
 
   useEffect(() => {
     if (globeEl.current) {
       globeEl.current.controls().autoRotate = true;
-      globeEl.current.controls().autoRotateSpeed = 1.2;
+      globeEl.current.controls().autoRotateSpeed = 1.4;
       globeEl.current.controls().enableZoom = false;
+    }
+    if (window.innerWidth < 700) {
+      setGlobeWidth(400);
+      setGlobeHeight(400);
     }
   }, []);
 
@@ -19,23 +25,24 @@ const SpinningGlobe = () => {
       ref={globeEl}
       globeImageUrl='/images/globe.jpg'
       pointsData={[
-        { lat: 30.0444, lng: 31.2357, color: '#9c27b0', altitude: 0 },
+        { lat: 30.0444, lng: 31.2357, color: '#f4a949', altitude: 0.018 },
       ]}
       pointAltitude='altitude'
       pointColor='color'
-      backgroundColor='rgba(0,0,0,0)'
-      width={850}
-      height={700}
+      width={globeWidth}
+      height={globeHeight}
       showAtmosphere={false}
-      pointRadius={1}
+      backgroundColor='#141416'
       labelsData={places}
       labelLat={(d) => d.latitude}
       labelLng={(d) => d.longitude}
       labelText={(d) => d.name}
+      labelColor={() => '#f0f8ff'}
+      pointRadius={0.8}
       labelSize={(d) => 3}
-      labelDotRadius={(d) => 1.5}
-      labelColor={() => '#c98202'}
-      labelResolution={5}
+      labelDotRadius={(d) => 0}
+      labelResolution={10}
+      labelAltitude={0.01}
     />
   );
 };
